bfe7310afad4d69e5ac835b84e5d3581cd34d41e
Kim-Seo-Gwon <tjrnjs123@gmail.com>
2022-11-16 23:58:15 +0900

    install iron-session, add lib, api directory

---
+++ lib/server/withSession.ts
@@ -0,0 +1,18 @@
+import { withIronSessionApiRoute } from "iron-session/next";
+
+declare module "iron-session" {
+  interface IronSessionData {
+    user?: {
+      id: number;
+    };
+  }
+}
+
+const cookieOptions = {
+  cookieName: "userSession",
+  password: process.env.COOKIE_PASSWORD!,
+};
+
+export function withSession(fn: any) {
+  return withIronSessionApiRoute(fn, cookieOptions);
+}
