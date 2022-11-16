25bed09da4b24f39ad996cf0641952c14af72c7b
Kim-Seo-Gwon <tjrnjs123@gmail.com>
2022-11-17 01:46:38 +0900

    로그인

---
+++ pages/api/users/signIn.tsx
@@ -0,0 +1,31 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@libs/server/client";
+import apiHandler from "@libs/server/apiHandler";
+import { withSession } from "@libs/server/withSession";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { account, password } = req.body;
+  console.log(req.body);
+
+  const foundUser = await client.user.findUnique({
+    where: {
+      id: account,
+    },
+  });
+
+  if (!foundUser) {
+    return res.json({ ok: false, message: "Invalid account" });
+  } else if (!foundUser.isAccept) {
+    return res.json({ ok: false, message: "No permission" });
+  } else if (foundUser.pwd !== password) {
+    return res.json({ ok: false, message: "Invalid password" });
+  } else {
+    req.session.user = {
+      account: foundUser.id,
+    };
+    req.session.save();
+    return res.json({ ok: true });
+  }
+}
+
+export default withSession(apiHandler("POST", handler));
