9999284a0e70af3f5c68e05417a5ab400035bce0
Seung-ho Kim <rhanfwlq980@naver.com>
2022-11-22 12:54:49 +0900

    [ADD] 게시판 페이지 (/bulletins) 제작

--- pages/bulletins/index.tsx
+++ pages/bulletins/index.tsx
@@ -1,14 +1,26 @@
 import type {NextPage} from 'next'
-import Layout from "../../components/layout";
+import Layout from "@components/Layout";
 import Head from "next/head";
+import Bulletin from "@components/Bulletin";
 
 const Bulletins: NextPage = () => {
   return (
     <Layout title={"게시판"} canGoBack hasTabBar>
       <Head><title>Bulletins</title></Head>
-      <div className={"flex flex-col space-y-5 divide-y"}>
-        게시판
-      </div>
+      <section className={"divide-y"}>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((_, i) => (
+          <Bulletin
+            key={i}
+            id={i}
+            title={"데이터베이스"}
+            content={"IC-PBL 최종 발표"}
+            createdAt={"2022-10-25"}
+            comments={1}
+            hearts={1}
+            userId={1}
+          />
+        ))}
+      </section>
     </Layout>
   );
 }
