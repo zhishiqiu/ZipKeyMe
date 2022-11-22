0aa8d1c49d33f175b8c6396fd881e30c0c63521e
Seung-ho Kim <rhanfwlq980@naver.com>
2022-11-22 16:00:36 +0900

    [ADD] 관리자 채팅 페이지 (/chats) 제작

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,13 +1,31 @@
 import type {NextPage} from 'next'
 import Layout from "@components/Layout";
 import Head from "next/head";
+import Message from "@components/Message";
 
 const Chats: NextPage = () => {
   return (
-    <Layout title={"관리자 채팅"} hasTabBar>
+    <Layout title={"관리자 채팅"} hasTabBar canGoBack>
       <Head><title>Chats</title></Head>
-      <div className={"flex flex-col space-y-5 divide-y"}>
-        관리자 채팅
+      <div className={"py-10 pb-16 px-4 space-y-4"}>
+        <Message message={"무엇을 도와드릴까요?"}/>
+        <Message message={"윗 집이 너무 쿵쿵거려요."} reversed/>
+        <Message message={"조치해드리겠습니다."}/>
+
+        <form className="fixed py-2 bg-white bottom-24 inset-x-0 mx-6">
+          <div className="flex relative max-w-md items-center w-full mx-auto">
+            <input
+              type="text"
+              className="shadow-sm rounded-full w-full border-gray-300 focus:ring-pantone focus:outline-none pr-12 focus:border-pantone"
+              placeholder={"메시지를 입력하세요."}
+            />
+            <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
+              <button className="flex focus:ring-2 focus:ring-offset-2 focus:ring-pantone items-center bg-pantone rounded-full px-3 text-sm text-white">
+                &rarr;
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     </Layout>
   );
