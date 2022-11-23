69a108a395a8e693c1ebf24a328711f6911a5df1
zhishiqiu <leiyating407@gmail.com>
2022-11-23 15:38:33 +0900

    car page 완성

--- pages/register.tsx
+++ pages/register.tsx
@@ -17,27 +17,27 @@ const Register: NextPage = () => {
 			<div className={"flex flex-col space-y-5 divide-y "}>
 				<form className="leading-10 ml-0.5" >
 				<h2 className="font-black">아이디</h2>
-				<p><input className="placeholder-gray-400 rounded border-gray-400" type="text" size={40}  name="text" placeholder="영문,소문자,숫자 조합" ></input></p>
+				<p><input className="placeholder-gray-300 rounded border-gray-300" type="text" size={40}  name="text" placeholder="영문,소문자,숫자 조합" ></input></p>
 				<h2 className="font-black">비밀번호</h2>
-				<p><input className="placeholder-gray-400 rounded border-gray-400" type="text" size={40} name="text" placeholder="8~20자리 영문,숫자 조합" ></input></p>
-				<p><input className="placeholder-gray-400 rounded border-gray-400" type="text"size={40} name="text" placeholder="다시 입력해 주세요" ></input></p>
+				<p><input className="placeholder-gray-300 rounded border-gray-300" type="text" size={40} name="text" placeholder="8~20자리 영문,숫자 조합" ></input></p>
+				<p><input className="placeholder-gray-300 rounded border-gray-300" type="text"size={40} name="text" placeholder="다시 입력해 주세요" ></input></p>
 				<h2 className="font-black">이름</h2>
-				<p><input className="placeholder-gray-400 rounded border-gray-400" type="text" size={40} name="text" placeholder="이름을 입력하세요" ></input></p>
+				<p><input className="placeholder-gray-300 rounded border-gray-300" type="text" size={40} name="text" placeholder="이름을 입력하세요" ></input></p>
 				<h2 className="font-black">생년월일</h2>
-				<p><input className="placeholder-gray-400 rounded border-gray-400" type="text" size={40} name="text" placeholder="생년월일을 입력해주세요,예:19990110" ></input></p>
+				<p><input className="placeholder-gray-300 rounded border-gray-300" type="text" size={40} name="text" placeholder="생년월일을 입력해주세요,예:19990110" ></input></p>
 				<h2 className="font-black">이메일</h2>
-				<p><input className="placeholder-gray-400 rounded border-gray-400" type="text" size={40} name="text" placeholder="이메일을 입력해주세요,예:111@gmail.com" ></input></p>
+				<p><input className="placeholder-gray-300 rounded border-gray-300" type="text" size={40} name="text" placeholder="이메일을 입력해주세요,예:111@gmail.com" ></input></p>
 				<h2 className="font-black">성별</h2>
 				
-				<p><input className="placeholder-gray-400 rounded border-gray-400" type="text"  size={17}name="text" placeholder="남성" ></input>&nbsp;&nbsp;&nbsp;
-					<input className="placeholder-gray-400 rounded border-gray-400" type="text" size={17} name="text" placeholder="여성" ></input>
+				<p><input className="placeholder-gray-300 rounded border-gray-300 text-center" type="text"  size={17}name="text" placeholder="남성" ></input>&nbsp;&nbsp;&nbsp;
+					<input className="placeholder-gray-300 rounded border-gray-300 text-center" type="text" size={17} name="text" placeholder="여성" ></input>
 				</p>
 				
 				<h2 className="font-black">휴대폰번호</h2>
-				<p><input className="placeholder-gray-400 rounded border-gray-400" type="text" size={40} name="text" placeholder="'-'를 제외한 숫자만 입력해주세요" ></input></p>
+				<p><input className="placeholder-gray-300 rounded border-gray-300" type="text" size={40} name="text" placeholder="'-'를 제외한 숫자만 입력해주세요" ></input></p>
 				<h2 className="font-black">주소</h2>
-				<p><input className="placeholder-gray-400 rounded border-gray-400" type="text"  size={17}name="text" placeholder="동(스크롤로 선택)" ></input>&nbsp;&nbsp;&nbsp;
-					<input className="placeholder-gray-400 rounded border-gray-400" type="text" size={17} name="text" placeholder="호(스크롤로 선택)" ></input></p>
+				<p><input className="placeholder-gray-300 rounded border-gray-300 text-center" type="text"  size={17}name="text" placeholder="동(스크롤로 선택)" ></input>&nbsp;&nbsp;&nbsp;
+					<input className="placeholder-gray-300 rounded border-gray-300 text-center" type="text" size={17} name="text" placeholder="호(스크롤로 선택)" ></input></p>
 				<h2 className="font-black">약관 동의</h2>
 				<div className='choose'>
 				<p><input className="w-6 h-6" type="checkbox" name="choose1" value="choose1"></input>&nbsp;&nbsp;
