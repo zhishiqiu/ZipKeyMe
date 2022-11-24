8b838d5d5ccfc1193b97acd541f9b888064d8a94
Kim-Seo-Gwon <tjrnjs123@gmail.com>
2022-11-24 13:23:35 +0900

    [ADD] 게시글API 뼈대 구현

---
+++ pages/api/bulletins/posts/index.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import apiHandler from "@libs/server/apiHandler";
+import client from "@libs/server/client";
+import { withSession } from "@libs/server/withSession";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method == "GET") {
+    const posts;
+    return res.json({ ok: true, posts });
+  }
+  if (req.method == "POST") {
+    const post;
+    return res.json({ ok: true, post });
+  }
+  if (req.method == "PATCH") {
+    const post;
+    return res.json({ ok: true, post });
+  }
+}
+
+export default withSession(
+  apiHandler({
+    method: ["GET", "POST", "PATCH"],
+    handler,
+    isPrivate: true,
+  })
+);
