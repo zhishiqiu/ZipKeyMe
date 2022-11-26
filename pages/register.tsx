148bcce2e651bce6e3f31d024d1d3fbf47660b06
Kim-Seo-Gwon <tjrnjs123@gmail.com>
2022-11-26 20:38:31 +0900

    [FEAT] 회원가입페이지(/register.tsx) 레지스터 연결 &  성별 선택 텍스트박스 -> 버튼

--- pages/register.tsx
+++ pages/register.tsx
@@ -3,14 +3,15 @@ import CopyRights from "public/copyRights.svg";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Layout from "@components/Layout";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import Button from "@components/Button";
 import { useRouter } from "next/router";
 import useMutation from "@libs/client/useMutation";
 
 interface SignUpForm {
   account: string;
   password: string;
-  checkPassword: string;
   name: string;
   birth: string;
   email: string;
@@ -29,12 +30,19 @@ interface MutationResult {
 const Register: NextPage = () => {
   const [signUp, { loading, data, error }] =
     useMutation<MutationResult>("/api/users/signUp");
-  const { register, handleSubmit, reset } = useForm<SignUpForm>();
+  const { register, handleSubmit, watch, reset } = useForm<SignUpForm>();
+  const [mustAgree, setMustAgree] = useState(true);
+  const [checkPassword, setCheckPassword] = useState("");
   const onValid = (validForm: SignUpForm) => {
-    if (loading) return;
+    // if (loading) return;
     // 형식적 validation
-    signUp(validForm);
+    // signUp(validForm);
+    console.log(validForm);
   };
+  useEffect(() => {
+    const check = watch((data) => console.log(data));
+  }, [watch]);
+
   return (
     <Layout
       title={"회원가입"}
@@ -45,17 +53,16 @@ const Register: NextPage = () => {
         <title>Register</title>
       </Head>
 
-      <div className={"flex flex-col space-y-5 divide-y "}>
+      <div className={"flex flex-col items-center mt-3 divide-y w-full"}>
         <form
           onSubmit={handleSubmit(onValid)}
-          className="leading-10 ml-0.5"
+          className="flex flex-col mx-2 space-y-2"
         >
           <h2 className="font-black">아이디</h2>
           <p>
             <input
-              className="placeholder-gray-300 rounded border-gray-300"
+              className="w-full placeholder-gray-300 rounded-xl border-gray-300"
               type="text"
-              size={40}
               placeholder="영문,소문자,숫자 조합"
               {...register("account", {
                 required: true,
@@ -65,7 +72,7 @@ const Register: NextPage = () => {
           <h2 className="font-black">비밀번호</h2>
           <p>
             <input
-              className="placeholder-gray-300 rounded border-gray-300"
+              className="w-full placeholder-gray-300 rounded-xl border-gray-300"
               type="password"
               size={40}
               placeholder="8~20자리 영문,숫자 조합"
@@ -78,19 +85,17 @@ const Register: NextPage = () => {
           </p>
           <p>
             <input
-              className="placeholder-gray-300 mt-3 rounded border-gray-300"
+              className="w-full placeholder-gray-300 mt-3 rounded-xl border-gray-300"
               type="password"
               size={40}
               placeholder="다시 입력해 주세요"
-              {...register("checkPassword", {
-                required: true,
-              })}
+              onChange={(data) => setCheckPassword(data.target.value)}
             ></input>
           </p>
           <h2 className="font-black">이름</h2>
           <p>
             <input
-              className="placeholder-gray-300 rounded border-gray-300"
+              className="w-full placeholder-gray-300 rounded-xl border-gray-300"
               type="text"
               size={40}
               placeholder="이름을 입력하세요"
@@ -102,119 +107,125 @@ const Register: NextPage = () => {
           <h2 className="font-black">생년월일</h2>
           <p>
             <input
-              className="placeholder-gray-300 rounded border-gray-300"
+              className="w-full placeholder-gray-300 rounded-xl border-gray-300"
               type="date"
               size={40}
               placeholder="날짜선택"
-              {...register("checkPassword", {
+              {...register("birth", {
                 required: true,
               })}
             ></input>
           </p>
-          <h2 className="font-black">이메일</h2>
+          <h2 className="font-black mx-2">이메일</h2>
           <p>
             <input
-              className="placeholder-gray-300 rounded border-gray-300"
+              className="w-full placeholder-gray-300 rounded-xl border-gray-300"
               type="email"
               size={40}
               placeholder="이메일을 입력해주세요,예:aaa111@gmail.com"
-              {...register("checkPassword", {
+              {...register("email", {
                 required: true,
               })}
             ></input>
           </p>
           <h2 className="font-black">성별</h2>
-
+          <div className="grid grid-cols-2 ext-lg items-center space-x-1">
+            <div>
+              <input
+                id="male"
+                className="peer/male hidden"
+                type="radio"
+                value="MALE"
+                checked
+                {...register("gender", {
+                  required: true,
+                })}
+              />
+              <label
+                htmlFor="male"
+                className="border border-gray-300 block cursor-pointer select-none rounded-xl p-2 text-center peer-checked/male:bg-pantone peer-checked/male:text-white"
+              >
+                남성
+              </label>
+            </div>
+            <div>
+              <input
+                id="female"
+                className="peer/female hidden"
+                type="radio"
+                value="FEMALE"
+                {...register("gender", {
+                  required: true,
+                })}
+              />
+              <label
+                htmlFor="female"
+                className="border border-gray-300 block cursor-pointer select-none rounded-xl p-2 text-center peer-checked/female:bg-pantone p peer-checked/female:text-white"
+              >
+                여성
+              </label>
+            </div>
+          </div>
+          <h2 className="font-black ">휴대폰번호</h2>
           <p>
             <input
-              className="placeholder-gray-300 rounded border-gray-300 text-center"
+              className="w-full placeholder-gray-300 rounded-xl border-gray-300"
               type="text"
-              size={17}
-              name="text"
-              placeholder="남성"
-            ></input>
-            &nbsp;&nbsp;&nbsp;
-            <input
-              className="placeholder-gray-300 rounded border-gray-300 text-center"
-              type="text"
-              size={17}
-              name="text"
-              placeholder="여성"
-            ></input>
-          </p>
-
-          <h2 className="font-black">휴대폰번호</h2>
-          <p>
-            <input
-              className="placeholder-gray-300 rounded border-gray-300"
-              type="text"
-              size={40}
               placeholder="'-'를 제외한 숫자만 입력해주세요"
-              {...register("checkPassword", {
+              {...register("phone", {
                 required: true,
               })}
             ></input>
           </p>
           <h2 className="font-black">주소</h2>
-          <p>
+          <p className="flex justify-between">
             <input
-              className="placeholder-gray-300 rounded border-gray-300 text-center"
+              className="w-1/2 placeholder-gray-300 justify-self-auto rounded-xl border-gray-300 text-center"
               type="text"
-              size={17}
+              size={18}
               placeholder="동"
-              {...register("checkPassword", {
+              {...register("aptDong", {
                 required: true,
               })}
             ></input>
             &nbsp;&nbsp;&nbsp;
             <input
-              className="placeholder-gray-300 rounded border-gray-300 text-center"
+              className="w-1/2 placeholder-gray-300 justify-self-auto rounded-xl border-gray-300 text-center"
               type="text"
-              size={17}
+              size={18}
               placeholder="호"
-              {...register("checkPassword", {
+              {...register("aptHo", {
                 required: true,
               })}
             ></input>
           </p>
           <h2 className="font-black">약관 동의</h2>
-          <div className="choose">
-            <p>
-              <input
-                className="w-6 h-6"
-                type="checkbox"
-                name="choose1"
-                value="choose1"
-              ></input>
-              &nbsp;&nbsp;
-              <label className="font-medium text-sm">
-                [필수]집키미 이용약관 및 개인정보처리방침 이용 동의
-              </label>
-            </p>
+          <label className="space-x-2">
             <input
-              className="w-6 h-6"
               type="checkbox"
-              name="choose2"
-              value="choose2"
-            ></input>
-            &nbsp;&nbsp;
-            <label className="font-medium text-sm">
-              [선택]푸시 알림 수신 동의
-            </label>
-          </div>
-          <div className="w-80 h-14 font-black bg-[#6667ab] ml-4 mt-3 rounded">
-            <p className="text-white text-center text-lg ">
-              <input
-                className="mt-3"
-                type="submit"
-                value="회원가입"
-              ></input>
-            </p>
-          </div>
-          <div className="mt-5 ml-14">
-            <CopyRights />
+              checked={mustAgree}
+              onChange={() => setMustAgree(!mustAgree)}
+            />
+            <span>[필수] 집키미 이용약관 및 개인정보처리방침 이용 동의</span>
+          </label>
+
+          <label className="space-x-2">
+            <input
+              type="checkbox"
+              {...register("agree", {})}
+            />
+            <span>[선택] 푸시 알람 수신 동의</span>
+          </label>
+          <div className="py-4 font-bold">
+            <Button
+              large
+              text={loading ? "회원가입 중" : "회원가입"}
+            />
           </div>
         </form>
+        <div className="flex flex-col items-center mb-2">
+          <CopyRights />
+        </div>
       </div>
     </Layout>
   );
